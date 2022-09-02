9a0ea685e0d4bcc02d0a3351980d042243a40cc2
Luiz Davi <luizdaviamorim.com@gmail.com>
2022-09-02 13:20:08 -0300

    vamos la

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e8/4026d596d72a001d108f90187b7f9760
@@ -0,0 +1,11 @@
+package com.adenda.agenda.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.adenda.agenda.entities.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+
+	Paciente findByCpf(String cpf);
+
+}
--- agenda/src/main/java/com/adenda/agenda/controller/PacienteController.java
+++ agenda/src/main/java/com/adenda/agenda/controller/PacienteController.java
@@ -1,11 +1,9 @@
 package com.adenda.agenda.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.adenda.agenda.copia.PacienteCopia;
 import com.adenda.agenda.dto.PacienteDto;
-import com.adenda.agenda.entities.Paciente;
 import com.adenda.agenda.service.Pacienteservice;
 
 
